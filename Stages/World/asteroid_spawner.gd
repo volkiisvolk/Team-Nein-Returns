10cb31888463f206ecdd24042d30b1608388ec51
Hannes Hofmeister <Hannes.hofmeister@uni-ulm.de>
2024-12-14 04:05:04 +0100

    fix Astorids ChunkManagment

--- Stages/World/asteroid_spawner.gd
+++ Stages/World/asteroid_spawner.gd
@@ -1,41 +1,70 @@
+# Asteroids Node Code
 extends Node2D
 
 @export var chunk_size = 512 # Definiert die Größe der Chunks
-@export var view_distance = 2 # Anzahl geladener Chunks um den Spieler herum
+@export var view_distance = 3 # Angepasst für Fullscreen (größerer Sichtbereich)
 
 var active_chunks = {} # Gespeicherte aktive Chunks
 var asteroid_scene = preload("res://Stages/World/Asteroids/asteroid.tscn")
+var player_node = null # Referenz auf den Spieler
+
+func _ready():
+	# Spieler-Node initialisieren
+	player_node = get_parent().get_parent().get_node("Ship")
 
 func _process(delta):
 	_update_chunks()
 
 func _update_chunks():
-	var player_position = get_parent().get_parent().get_node("Ship").global_position
+	if player_node == null:
+		return # Sicherstellen, dass der Spieler existiert
+
+	var player_position = player_node.global_position
 	var current_chunk = Vector2(floor(player_position.x / chunk_size), floor(player_position.y / chunk_size))
 
+	# Speichere vorhandene Chunks
 	for x in range(current_chunk.x - view_distance, current_chunk.x + view_distance + 1):
 		for y in range(current_chunk.y - view_distance, current_chunk.y + view_distance + 1):
 			var chunk_key = Vector2(x, y)
 			if not active_chunks.has(chunk_key):
 				_load_chunk(chunk_key)
 
+	# Behalte alle Chunks in Reichweite
+	var chunks_to_keep = {}
 	for chunk_key in active_chunks.keys():
-		if chunk_key.distance_to(current_chunk) > view_distance:
+		if chunk_key.distance_to(current_chunk) <= view_distance:
+			chunks_to_keep[chunk_key] = active_chunks[chunk_key]
+		else:
+			_unload_chunk(chunk_key)
+
+	active_chunks = chunks_to_keep
+
+	# Deaktiviere entfernte Chunks
+	for chunk_key in active_chunks.keys():
+		if not chunks_to_keep.has(chunk_key):
 			_unload_chunk(chunk_key)
 
 func _load_chunk(chunk_key):
 	var chunk_position = chunk_key * chunk_size
+
+	# Falls der Chunk bereits existiert, nichts tun
+	if active_chunks.has(chunk_key):
+		for asteroid in active_chunks[chunk_key]:
+			asteroid.visible = true
+		return
+
 	active_chunks[chunk_key] = []
 
 	# Spawn einzelner oder clusterweise Asteroiden
-	for i in range(randi_range(3, 8)):
+	for i in range(randi_range(5, 10)): # Mehr Asteroiden für größere Sichtweite
 		var asteroid = asteroid_scene.instantiate()
 		asteroid.position = chunk_position + Vector2(randi_range(0, chunk_size), randi_range(0, chunk_size))
 		add_child(asteroid)
 		active_chunks[chunk_key].append(asteroid)
 
 func _unload_chunk(chunk_key):
+	# Verstecke Chunks, anstatt sie zu löschen
 	if active_chunks.has(chunk_key):
 		for asteroid in active_chunks[chunk_key]:
-			asteroid.queue_free()
-		active_chunks.erase(chunk_key)
+			if asteroid and is_instance_valid(asteroid):
+				asteroid.visible = false
--- Stages/World/background.gd
+++ Stages/World/background.gd
@@ -4,6 +4,7 @@ func _ready():
 	randomize()
 	_generate_starfield()
 
+
 func _generate_starfield():
 	var parallax_layer = get_node("Stars")
 	var star_texture = preload("res://icon.svg") # Sternenbild Textur
@@ -11,5 +12,6 @@ func _generate_starfield():
 	for i in range(star_count):
 		var star = Sprite2D.new()
 		star.texture = star_texture
-		star.position = Vector2(randi_range(-2000, 2000), randi_range(-2000, 2000))
+		star.position = Vector2(randi_range(-4000, 4000), randi_range(-4000, 4000)) # Größerer Bereich für Fullscreen
+		star.scale = Vector2(randi_range(5, 15) * 0.1, randi_range(5, 15) * 0.1) # Zufällige Größen für Sterne
 		parallax_layer.add_child(star)
