c02153d540f17add7211579ba10512cd49a7297b
David Jenzer <david.jenzer@uni-ulm.com>
2024-12-14 01:12:01 +0100

    feature added: Highscore

--- Global/Global.gd
+++ Global/Global.gd
@@ -1,5 +1,13 @@
 extends Node
 
+# Globale Variable für den Highscore
+var highscore: int = 0
+
+# Methode zum Aktualisieren des Highscores
+func update_highscore(score: int) -> void:
+	if score > highscore:
+		highscore = score
+		print("Neuer Highscore: %d" % highscore)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
